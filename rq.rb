f4c56da11c92ba14f81584e161aff90a7f7bd2e3
David Flemström <dflemstr@users.noreply.github.com>
2016-11-23 13:16:09 +0100

    Update rq to 0.9.1

--- rq.rb
+++ rq.rb
@@ -1,9 +1,9 @@
 class Rq < Formula
   desc "Record Query - A tool for doing record analysis and transformation"
   homepage "https://github.com/dflemstr/rq"
-  url "https://github.com/dflemstr/rq/archive/v0.9.0.tar.gz"
-  version "0.9.0"
-  sha256 "d10f78ae8e83d4c810f4a8ed22206388f843e9da7699d708bfbea10024e49fbd"
+  url "https://github.com/dflemstr/rq/archive/v0.9.1.tar.gz"
+  version "0.9.1"
+  sha256 "e93b6800ef7a7c5915af803d909c9924a420c4b9a36d8f590cfab953957ece4b"
 
   head "https://github.com/dflemstr/rq.git"
 
